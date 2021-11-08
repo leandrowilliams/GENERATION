0e5cdceae84dbc214ca7b2296e8cd28e7eaae391
leandrowilliams <87885115+leandrowilliams@users.noreply.github.com>
2021-11-08 10:19:12 -0300

    Game Store

---
+++ Spring React/Spring-boot/GameStore/gamestore/src/main/java/br/org/generation/gamestore/model/Categoria.java
@@ -0,0 +1,50 @@
+package br.org.generation.gamestore.model;
+
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@Entity
+
+@Table(name = "tb_categoria")
+public class Categoria {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)	
+	private long id;
+	
+	@NotNull(message = "Defina um tipo para o jogo.")
+	@Size(max=500)
+	private String tipo;
+	
+	@OneToMany(mappedBy = "categoria", cascade = CascadeType.ALL)
+	@JsonIgnoreProperties("categoria")
+	private List<Produto> produto;
+	
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+
+}
---
+++ Spring React/Spring-boot/GameStore/gamestore/src/test/java/br/org/generation/gamestore/GamestoreApplicationTests.java
@@ -0,0 +1,13 @@
+package br.org.generation.gamestore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamestoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
