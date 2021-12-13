ed7b3041a9b287ff5e4800594867afcb734fac76
leandrowilliams <87885115+leandrowilliams@users.noreply.github.com>
2021-12-13 00:53:44 -0300

    blog-pessoal

---
+++ Fundamentos Web/React/blogpessoal/blog-pessoal/src/paginas/home/Home.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Home.css';
+
+function Home(){
+    return (
+        <>
+            <h1>Home</h1>
+            <img src="https://i.imgur.com/fE2kDkZ.jpg" alt="Imagem Tela Inicial" width="800" height="500" />
+        </>
+    );
+}
+
+export default Home;
